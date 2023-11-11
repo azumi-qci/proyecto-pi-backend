db9a860b79c35f2a4a764ac3cd0f2656d608b0ad
Alejandro Suárez <alejandro-hdez115@outlook.com>
2023-11-11 10:09:03 -0600

    Add update access log route

--- routes/access.route.js
+++ routes/access.route.js
@@ -117,4 +117,103 @@ router.post('/:id', verifyToken, async (req, res) => {
   }
 });
 
+router.put('/edit/:id', verifyToken, async (req, res) => {
+  const { id } = req.params;
+  const { type } = req.user;
+
+  if (type !== config.ADMIN_LEVEL) {
+    return res.status(401).json({
+      error: true,
+      message: 'The user does not have the required priviligies',
+    });
+  }
+
+  const {
+    name,
+    car_brand,
+    car_color,
+    car_plate,
+    entrance_hour,
+    entrance_day,
+    id_door,
+    visit_location,
+  } = req.body;
+
+  if (
+    !name ||
+    !car_brand ||
+    !car_color ||
+    !car_plate ||
+    !entrance_hour ||
+    !entrance_day ||
+    !id_door ||
+    !visit_location
+  ) {
+    return res.status(422).json({
+      error: true,
+      message: 'Some required fields are not present in body',
+    });
+  }
+
+  try {
+    const query = await db.query(
+      `
+      UPDATE access
+      SET
+        name = ?,
+        car_brand = ?,
+        car_color = ?,
+        car_plate = ?,
+        entrance_hour = ?,
+        entrance_day = ?,
+        id_door = ?,
+        visit_location = ?
+      WHERE id = ?
+    `,
+      [
+        name,
+        car_brand,
+        car_color,
+        car_plate,
+        entrance_hour,
+        entrance_day,
+        id_door,
+        visit_location,
+        id,
+      ]
+    );
+
+    if (query.affectedRows === 0) {
+      return res.status(404).json({
+        error: true,
+        message: 'The access log was not found',
+      });
+    }
+
+    io.to(`door-${id_door}`).emit('update-log', {
+      id: parseInt(id),
+      name,
+      car_brand,
+      car_color,
+      car_plate,
+      entrance_hour,
+      entrance_day,
+      id_door,
+      visit_location,
+    });
+
+    return res.json({
+      error: false,
+      message: 'The access log was updated successfully',
+    });
+  } catch (error) {
+    console.warn(error);
+
+    return res.status(500).json({
+      error: true,
+      message: 'An error ocurred in server',
+    });
+  }
+});
+
 module.exports = router;
